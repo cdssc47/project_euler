6b18363b4dadd4c3016a3af7743a3a4f02d098d8
Colin <ColCarroll@users.noreply.github.com>
2014-01-05 13:13:00 -0600

    Update problem1.py

--- project_euler/problem1/problem1.py
+++ project_euler/problem1/problem1.py
@@ -4,6 +4,16 @@ __author__ = 'carterdelloro'
 
 def solve(n):
     return sum(set(range(3,n,3)+range(5,n,5)))
+    
+def solve2(n):
+    tot = 0
+    for j in range(1, n):
+        if j % 3 == 0 or j % 5 == 0:
+            tot += j
+    return tot
+
+def solve3(n):
+    return sum(j for j in range(1, n) if j % 3 == 0 or j % 5 == 0)
 
 def __main():
     print(solve(1000))
